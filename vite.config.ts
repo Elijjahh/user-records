a1e7c612c12dd728515097785327fc6e486da42f
Илья Белов <ibelov292005@gmail.com>
2025-03-17 22:46:46 +0300

    ci: добавил настройки для деплоя на gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
-import autoprefixer from 'autoprefixer'
-import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer';
+import tailwind from 'tailwindcss';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,10 +14,11 @@ export default defineConfig({
       plugins: [tailwind(), autoprefixer()],
     },
   },
+  base: '/user-records/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+});
